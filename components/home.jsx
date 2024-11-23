5516e0cc0c280051f47cfa9e89bbc19e2f45a7ca
Thandolwethu <thandwem045@gmail.com>
2024-11-23 23:55:29 +0200

    Created mock api

--- components/home.jsx
+++ components/home.jsx
@@ -3,45 +3,26 @@ import BlogList from "../src/BlogList"
 
 function Home () {
     
-  const [blogs, setBlogs ] = useState([
-
-    {
-      id: 1,
-      title: "Understanding JavaScript Closures",
-      author: "Mario",
-      content: "Closures are a powerful concept in JavaScript, enabling functions to access variables from their scope.",
-      date: "2024-11-21",
-      tags: ["JavaScript", "Programming", "Web Development"],
-    },
-    {
-      id: 2,
-      title: "10 Tips for Better Web Accessibility",
-      author: "John Smith",
-      content: "Creating accessible websites ensures inclusivity and a better user experience for everyone.",
-      date: "2024-11-20",
-      tags: ["Accessibility", "Web Development", "Best Practices"],
-    },
-    {
-      id: 3,
-      title: "CSS Grid vs Flexbox: When to Use Which",
-      author: "Mario",
-      content: "Both Grid and Flexbox are essential CSS tools, but knowing when to use each can streamline your layouts.",
-      date: "2024-11-18",
-      tags: ["CSS", "Web Design", "Frontend Development"],
-    },
-  ])
+  // initializes our blogs as null
+  const [blogs, setBlogs ] = useState(null)
 
    const handleDelete = (id) => {
      const newBlogs = blogs.filter( blog => blog.id !== id)
      setBlogs(newBlogs);
    }
 
+  // fetches from our JSON-server 
    useEffect(() => {
-    // console.log("Use Effect Ran");
-   })
+    fetch("http://localhost:8000/blogs")
+     .then(res => { return res.json() })
+     .then((data) => {
+      setBlogs(data)
+     })
+   }, [])
+
   return (
     <div className="home">
-      <BlogList blogs={blogs}  title="All Blogs"  handleDelete={handleDelete} />
+      { blogs && <BlogList blogs={blogs}  title="All Blogs" handleDelete={handleDelete} />}
   </div>
 );
 }
